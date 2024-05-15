5db164dc09ccc34af6d339125e0dd9264eabea69
xjqxziii <xjqxziii@163.com>
2024-05-15 23:11:06 +0800

    rm speedtest

--- scripts/diy.sh
+++ scripts/diy.sh
@@ -3,5 +3,5 @@
 #更改默认地址为192.168.6.1
 sed -i 's/192.168.1.1/192.168.6.1/g' package/base-files/files/bin/config_generate
 
-git clone https://github.com/ZeaKyX/luci-app-speedtest-web.git package/luci-app-speedtest-web
-git clone https://github.com/ZeaKyX/speedtest-web.git package/speedtest-web
+#git clone https://github.com/ZeaKyX/luci-app-speedtest-web.git package/luci-app-speedtest-web
+#git clone https://github.com/ZeaKyX/speedtest-web.git package/speedtest-web
