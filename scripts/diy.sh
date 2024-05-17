cec82542d34d158bd6a5df87ffb73969ef750847
xjqxziii <xjqxziii@163.com>
2024-05-17 11:23:01 +0800

    kiddin9

--- scripts/diy.sh
+++ scripts/diy.sh
@@ -3,5 +3,4 @@
 #更改默认地址为192.168.6.1
 sed -i 's/192.168.1.1/192.168.6.1/g' package/base-files/files/bin/config_generate
 
-git clone https://github.com/ZeaKyX/luci-app-speedtest-web.git package/luci-app-speedtest-web
-git clone https://github.com/ZeaKyX/speedtest-web.git package/speedtest-web
+echo 'src-git kiddin9 https://github.com/kiddin9/openwrt-packages.git' >>feeds.conf.default
