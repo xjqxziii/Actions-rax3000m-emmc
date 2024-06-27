0a228020cc5d272f398295dd49450227dbeff439
xjqxziii <xjqxziii@163.com>
2024-06-27 10:37:47 +0800

    remove speedtest

--- scripts/diy.sh
+++ scripts/diy.sh
@@ -3,4 +3,4 @@
 #更改默认地址为192.168.6.1
 sed -i 's/192.168.1.1/192.168.6.1/g' package/base-files/files/bin/config_generate
 
-echo 'src-git kiddin9 https://github.com/kiddin9/openwrt-packages.git' >>feeds.conf.default
+#echo 'src-git kiddin9 https://github.com/kiddin9/openwrt-packages.git' >>feeds.conf.default
